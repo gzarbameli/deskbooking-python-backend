cb55b14a20814d1905263c2f7fdd1501e6c9489f
Giacomo Zarba Meli <giacomo.zarba@immobiliare.it>
2023-12-02 16:12:44 +0100

    fix mysql connection issues

--- app.py
+++ app.py
@@ -80,20 +80,17 @@ def book():
 
         if room and date and time_s and time_e:
             cursor = mysql_connection.cursor()
+            
             cursor.execute("SELECT room_id FROM room WHERE name = %s", (room,))
             room_id = cursor.fetchone()[0]
             app.logger.info(str(room_id))
-            cursor.close()
 
-            cursor = mysql_connection.cursor()
             cursor.execute("INSERT INTO reservations (employee_id, date, starting_time, ending_time, room_id) VALUES (%s, %s, %s, %s, %s)", (employee_id, date, time_s, time_e, room_id))
-            cursor.close()
 
-            cursor = mysql_connection.cursor()
             cursor.execute("SELECT * FROM reservations")
             results = cursor.fetchall()
+            
             cursor.close()
-
             mysql_connection.close()
             app.logger.info("Reservation correctly registered")
             return jsonify({"token": "123"})
